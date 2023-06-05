5c5ef4f84de5ce8bbb157cde680609f2cff10f16
admin-n786 <movahhedd786@gmail.com>
2023-06-05 11:59:13 -0700

    second commit add picture to this project

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.red,
         appBar: AppBar(
           backgroundColor: Colors.blueGrey,
           title: Center(child: Text("I Am rich")),
         ),
         body: Center(
           child: Image(
-            image: AssetImage('images/s.jpg'),
+            image: AssetImage('images/download.jpg'),
           ),
         ),
       ),
